213aa20e3290464cb288da91ad2d5290427f891f
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-12-06 22:58:01 +0700

    [Coding] Make the ship wrap
    - Adding GameInitialize
    - Adding ScreenUtils
    - Adding Circle Collider 2D
    - Make the ship wrap

--- Asteroids/Assets/Scripts/Ship.cs
+++ Asteroids/Assets/Scripts/Ship.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-    #region
+    #region Fields
     Rigidbody2D _rb;
+    float _radius;
     Vector2 thrustDirection = new Vector2(1, 0);
 
     [SerializeField]
@@ -15,12 +16,19 @@ public class Ship : MonoBehaviour
     void Start()
     {
         _rb = this.GetComponent<Rigidbody2D>();
+        CircleCollider2D _circleCollider = this.GetComponent<CircleCollider2D>();
+        _radius = _circleCollider.radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = this.transform.position;
+
+        if (pos.x - _radius >= ScreenUtils.ScreenRight)
+        {
+            this.transform.position = new Vector3(ScreenUtils.ScreenLeft, pos.y, pos.z);
+        }
     }
 
     private void FixedUpdate()
