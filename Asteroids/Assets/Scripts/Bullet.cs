2cf535c64c6c89aacdfb8a33e5e2eee9d40978f1
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-12-18 14:49:03 +0700

    [Coding] Destroy bullets after a certain period of time

--- Asteroids/Assets/Scripts/Bullet.cs
+++ Asteroids/Assets/Scripts/Bullet.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private float _bulletSpeed = 100.0f;
+    private float _bulletSpeed = 200.0f;
+    private float _bulletLifeTime = 2.0f;
+    Timer timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = this.AddComponent<Timer>();
+        timer.Duration = _bulletLifeTime;
+        timer.Run();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer.Finished)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void ApplyForce(Vector2 direction)
